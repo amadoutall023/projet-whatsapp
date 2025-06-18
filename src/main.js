bb5733737d7f72b02dd8094a5c788f2846be6c82
amadou Tall <elhadjiamadoutall85@gmail.com>
2025-06-18 19:32:47 +0000

    last2 commit

--- src/main.js
+++ src/main.js
@@ -282,7 +282,7 @@ function logout() {
 
 async function afficherGroupesDansContactsList() {
   try {
-    const response = await fetch(`${API_BASE_URL}/groups`);
+    const response = await fetch(`https://json-server-lt3n.onrender.com/groups`);
     if (!response.ok) throw new Error("Endpoint /groups introuvable");
 
     const groupes = await response.json();
